524e83d98c08b18d9a8ab84dfa5a2d582e5746d0
Sylwia Gryglicka <sylwi4gryglicka@hotmail.com>
2020-01-08 21:58:40 +0100

    edited fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,23 @@
-print('hello world!')
+number = input('Podaj liczbę')
+
+for char in number:
+    if char == '0':
+        print('zero')
+    elif char == '1':
+        print('jeden')
+    elif char == '2':
+        print('dwa')
+    elif char == '3':
+        print('trzy')
+    elif char == '4':
+        print('cztery')
+    elif char == '5':
+        print('pięć')
+    elif char == '6':
+        print('sześć')
+    elif char == '7':
+        print('siedem')
+    elif char == '8':
+        print('osiem')
+    elif char == '9':
+        print('dziewięć')
